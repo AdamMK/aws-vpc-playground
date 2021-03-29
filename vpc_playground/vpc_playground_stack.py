356c4375cf08c256274dc90d42058bb96c03bff2
Adam Kozlowski <adamkozlowski0@gmail.com>
2021-03-29 16:56:29 +0100

    Initial

--- vpc_playground/vpc_playground_stack.py
+++ vpc_playground/vpc_playground_stack.py
@@ -1,4 +1,5 @@
-from aws_cdk import core
+from aws_cdk import (aws_ec2 as ec2, core)
+from aws_cdk.core import Tags
 
 
 class VpcPlaygroundStack(core.Stack):
@@ -6,4 +7,17 @@ class VpcPlaygroundStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        vpc = ec2.Vpc(
+        self, "Adam_MyVpc",
+        cidr="10.13.0.0/21",
+        max_azs=2,
+        nat_gateways=1,
+        subnet_configuration=[
+            ec2.SubnetConfiguration(name="public", cidr_mask=24, subnet_type=ec2.SubnetType.PUBLIC),
+            ec2.SubnetConfiguration(name="private", cidr_mask=24, subnet_type=ec2.SubnetType.PRIVATE),
+            ec2.SubnetConfiguration(name="isolated", cidr_mask=24, subnet_type=ec2.SubnetType.ISOLATED)
+        ]
+    )    
+
+        Tags.of(vpc).add("Owner", "Adam")
+    
